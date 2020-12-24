ca57e19c88f93279afe2861c249955d21d5f5017
mirkotomovic <mirko.mire.tomovic@gmail.com>
2020-12-25 00:12:28 +0100

    widget scaling fix

--- frontend/src/components/CoinList/CoinDetails/Widget/Widget.js
+++ frontend/src/components/CoinList/CoinDetails/Widget/Widget.js
@@ -8,21 +8,56 @@ const Widget = ({ crypto }) => {
     Monero: 6,
   };
   return (
-    <div className='widget-container'>
+    <div
+      style={{
+        height: '560px',
+        backgroundColor: '#ffffff',
+        overflow: 'hidden',
+        boxSizing: 'border-box',
+        border: '1px solid #56667f',
+        borderRadius: '4px',
+        textAlign: 'right',
+        lineHeight: '14px',
+        fontSize: '12px',
+        fontFeatureSettings: 'normal',
+        textSizeAdjust: '100%',
+        boxShadow: 'inset 0 -20px 0 0 #56667f',
+        padding: '1px',
+        padding: '0px',
+        margin: '0px',
+        width: '100%',
+      }}
+    >
       <div className='widget-content'>
         <iframe
-          src={`https://widget.coinlib.io/widget?type=chart&theme=light&coin_id=${cryptos[crypto]}&pref_coin_id=1506`}
+          // src={`https://widget.coinlib.io/widget?type=chart&theme=light&coin_id=${cryptos[crypto]}&pref_coin_id=1506`}
           scrolling='auto'
           className='widget-iframe'
           title={crypto}
         ></iframe>
       </div>
-      <div className='widget-footer'>
+      <div
+        style={{
+          color: '#ffffff',
+          lineHeight: '14px',
+          fontWeight: '400',
+          fontSize: '11px',
+          boxSizing: 'border-box',
+          padding: '2px 6px',
+          width: '100%',
+          fontFamily: 'Verdana, Tahoma, Arial, sans-serif',
+        }}
+      >
         <a
           href='https://coinlib.io'
           target='_blank'
           rel='noreferrer'
-          className='widget-copyright-link'
+          style={{
+            fontWeight: '500',
+            color: '#ffffff',
+            textDecoration: 'none',
+            fontSize: '11px',
+          }}
         >
           Cryptocurrency Prices
         </a>
